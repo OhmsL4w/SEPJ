18bd5a376024dc9bb5b3bf6d3947654c4f7ea155
Colby McDonald <67612969+Colby-M@users.noreply.github.com>
2023-04-18 15:25:21 -0400

    Database stuff
    Created a database, created a connection, made the first query to see if the login is successful or not. Still deciding whether or not there should be a username or just the UserID.

---
+++ Air3550/Login.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Login
+{
+    public static void LoginMethod()
+    {
+        // get the username and password from the inputs
+        Console.WriteLine("Input Username");
+        string? username = Console.ReadLine();
+        Console.WriteLine("Input Password");
+        string? password = Console.ReadLine();
+        if(username == null || password == null)
+        {
+            Console.WriteLine("Please enter in a correct Username and Password");
+            // retry it
+            LoginMethod();
+            return;
+        }
+        // hash the password using SHA-512
+        byte[] hashByte;
+        var data = Encoding.UTF8.GetBytes(password);
+        using (SHA512 shaM = new SHA512Managed())
+        {
+            hashByte = shaM.ComputeHash(data);
+        }
+        // Have the byte array of the hash, convert it to a string, replace the - with a blank after
+        string hash = BitConverter.ToString(hashByte);
+        hash = hash.Replace("-", "");
+        //Console.WriteLine(hash);
+
+        // check against the database for the user with that username
+        bool loginSuccess = TryLogin(username, hash);
+        if(!loginSuccess)
+        {
+            Console.WriteLine("Invalid Username or Password!");
+            Console.WriteLine("1. Try again");
+            Console.WriteLine("2. Back");
+            string? input = Console.ReadLine();
+            switch(input)
+            {
+                case "1":
+                    LoginMethod();
+                    return;
+                case "2":
+                    return;
+                default:
+                    return;
+            }
+        }
+        //LoggedIn(username); // give the username to the function to find the user again
+        return;
+    }
+
+    public static void CreateAccount()
+    {
+        return;
+    }
+
+    public static bool TryLogin(string username, string hash)
+    {
+        // open the database
+        // "jdbc:sqlserver://localhost;integratedSecurity=true;encrypt=false"
+        using (SqlConnection sqlConn = new SqlConnection("Data Source=(local);Database=Air3550;Integrated Security=true;"))
+        {
+            sqlConn.Open();
+            string queryString = "SELECT TOP 1 Password FROM Users WHERE Users.UserID =" + "\'" + username + "\'";
+            SqlCommand query = new SqlCommand(queryString, sqlConn);
+            using (SqlDataReader reader = query.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    // get the hashed password saved in the database and check in against the inputted password
+                    string usersHash = reader.GetString(0);
+                    if(usersHash == hash)
+                    {
+                        return true;
+                    }
+                }
+            }
+            sqlConn.Close();
+        }
+        return false;
+    }
+}
--- Air3550/Program.cs
+++ Air3550/Program.cs
@@ -1,6 +1,6 @@
-﻿using System.Security.Cryptography;
-using System;
+﻿using System;
 using System.Text;
+
 
 namespace Air3550;
 class Program
@@ -14,6 +14,7 @@ class Program
             Console.WriteLine("Input a number to continue");
             Console.WriteLine("1. Login");
             Console.WriteLine("2. Quit");
+            Console.WriteLine("3. Create Account");
             string? input = Console.ReadLine();
             if(input == null |( input != "1" & input != "2"))
             {
@@ -23,34 +24,14 @@ class Program
             switch (input)
             {
                 case "1":
-                    Login();
+                    Login.LoginMethod();
                     break;
                 case "2":
                     return;
+                case "3":
+                    Login.CreateAccount();
+                    break;
             }
         }
-    }
-    static void Login()
-    {
-        // get the username and password from the inputs
-        Console.WriteLine("Input Username");
-        string? username = Console.ReadLine();
-        Console.WriteLine("Input Password");
-        string? password = Console.ReadLine();
-        // hash the password using SHA-512
-        byte[] hashByte;
-        var data = Encoding.UTF8.GetBytes(password ?? "");
-        using (SHA512 shaM = new SHA512Managed())
-        {
-            hashByte = shaM.ComputeHash(data);
-        }
-        // Have the byte array of the hash, convert it to a string, replace the - with a blank after
-        string hash = BitConverter.ToString(hashByte);
-        hash = hash.Replace("-", "");
-        //Console.WriteLine(hash);
-
-        // check against the database for the user with that username
-        tryLogin(username, hash);
-        return;
     }
 }
