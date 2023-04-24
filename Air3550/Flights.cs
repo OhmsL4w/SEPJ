3e598ea0e55b8dbf699ea551ad0d0b6847de5b64
Colby McDonald <67612969+Colby-M@users.noreply.github.com>
2023-04-24 15:44:51 -0400

    Got connection and pricing to work

--- Air3550/Flights.cs
+++ Air3550/Flights.cs
@@ -526,7 +526,7 @@ namespace Air3550
         public void AddFlight()
         {
             Console.WriteLine("Please enter all of the information");
-            Console.WriteLine("Input a new Flight Number");
+            Console.WriteLine("Input a Flight Number");
             string? nfns = Console.ReadLine();
             int nfn;
             while (true)
@@ -540,48 +540,47 @@ namespace Air3550
                 {
                     break;
                 }
-                Console.WriteLine("Input a new Flight Number");
+                Console.WriteLine("Input a Flight Number");
                 nfns = Console.ReadLine();
             }
-
-            Console.WriteLine("Input a new Origin Airport");
-            string? noa = Console.ReadLine();
-            while (noa == null | noa?.Length != 3)
+            string? noa;
+            using (SqlConnection sqlConn = new SqlConnection("Server=34.162.94.248; Database=air3550; Uid=sqlserver; Password=123;"))
             {
-                Console.WriteLine("Input a correct airport code please");
-
-                Console.WriteLine("Input a new Airport Code");
-                noa = Console.ReadLine();
-            }
-            Console.WriteLine("Input a new Destination Airport");
-            string? nda = Console.ReadLine();
-            while (nda == null | nda?.Length != 3)
-            {
-                Console.WriteLine("Input a correct airport code please");
-
-                Console.WriteLine("Input a new Airport Code");
-                nda = Console.ReadLine();
-            }
-
-            Console.WriteLine("Input a new Price (xx.xx)");
-            string? nps = Console.ReadLine();
-            decimal np;
-            while (true)
-            {
-                if (!decimal.TryParse(nps, out np))
+                sqlConn.Open();
+                do
                 {
-                    Console.WriteLine("Could not parse the decimal!");
-                    Console.WriteLine("Input a correct Price please");
-                }
-                else
+                    Console.WriteLine("Input a Origin Airport");
+                    noa = Console.ReadLine();
+                    string queryString = $"SELECT * FROM Airports WHERE Code=\'{noa}\'";
+                    SqlCommand query = new SqlCommand(queryString, sqlConn);
+                    SqlDataReader airports = query.ExecuteReader();
+                    if (airports.HasRows)
+                    {
+                        break;
+                    }
+                } while (true);
+                sqlConn.Close();
+            }
+            string? nda;
+            using (SqlConnection sqlConn = new SqlConnection("Server=34.162.94.248; Database=air3550; Uid=sqlserver; Password=123;"))
+            {
+                sqlConn.Open();
+                do
                 {
-                    break;
-                }
-                Console.WriteLine("Input a new Price (xx.xx)");
-                nps = Console.ReadLine();
+                    Console.WriteLine("Input a Destination Airport");
+                    nda = Console.ReadLine();
+                    string queryString = $"SELECT * FROM Airports WHERE Code=\'{nda}\'";
+                    SqlCommand query = new SqlCommand(queryString, sqlConn);
+                    SqlDataReader airports = query.ExecuteReader();
+                    if (airports.HasRows)
+                    {
+                        break;
+                    }
+                } while (true);
+                sqlConn.Close();
             }
 
-            Console.WriteLine("Input a new Departure Date (mm/dd/yyyy HH:MM:SS)");
+            Console.WriteLine("Input a Departure Date (mm/dd/yyyy HH:MM:SS)");
             string? ndds = Console.ReadLine();
             DateTime ndd;
             while (true)
@@ -595,12 +594,12 @@ namespace Air3550
                 {
                     break;
                 }
-                Console.WriteLine("Input a new Departure Date (mm/dd/yyyy HH:MM:SS)");
+                Console.WriteLine("Input a Departure Date (mm/dd/yyyy HH:MM:SS)");
                 ndds = Console.ReadLine();
             }
             SqlDateTime sqlndd = new SqlDateTime(ndd.Year, ndd.Month, ndd.Day, ndd.Hour, ndd.Minute, ndd.Second);
-
-            Console.WriteLine("Input a new Arrival Date and Time (mm/dd/yyyy HH:MM:SS)");
+            
+            Console.WriteLine("Input an Arrival Date and Time (mm/dd/yyyy HH:MM:SS)");
             string? nads = Console.ReadLine();
             DateTime nad;
             while (true)
@@ -608,37 +607,246 @@ namespace Air3550
                 if (!DateTime.TryParse(nads, out nad))
                 {
                     Console.WriteLine("Could not parse the date and time!");
-                    Console.WriteLine("Input a correct Date and time in the format given no paranthesis please");
+                    Console.WriteLine("Input a correct Date and time in the format given no parenthesis please");
                 }
                 else
                 {
                     break;
                 }
-                Console.WriteLine("Input a new Departure Date (mm/dd/yyyy HH:MM:SS)");
+                Console.WriteLine("Input an Arrival Date (mm/dd/yyyy HH:MM:SS)");
                 nads = Console.ReadLine();
             }
 
-            SqlDateTime sqlnad = new SqlDateTime(nad.Year, nad.Month, nad.Day, nad.Hour, nad.Minute, nad.Second);
             using (SqlConnection sqlConn = new SqlConnection("Server=34.162.94.248; Database=air3550; Uid=sqlserver; Password=123;"))
-                { 
-                        sqlConn.Open();
+            {
+                sqlConn.Open();
+                // first find the distance of the two airports to be able to get the price
+                string distanceQuery = $"SELECT Distance FROM Distances WHERE (AirportA = \'{noa}\' AND AirportB = \'{nda}\') OR (AirportA = \'{nda}\' AND AirportB = \'{noa}\')";
+                SqlCommand distanceCommand = new SqlCommand(distanceQuery, sqlConn);
+                SqlDataReader distanceReader = distanceCommand.ExecuteReader();
+                if (!distanceReader.HasRows)
+                {
+                    Console.WriteLine("Could not find airports distances!");
+                    return;
+                }
+                List<int> distances = new List<int>();
+                while(distanceReader.Read())
+                {
+                    distances.Add(distanceReader.GetInt32(0));
+                }
+                distanceReader.Close();
+                decimal np = 50M;
+                // the only flight that needs a connection is anything to LAX, first must go to MSP or DEN
+                string? con = null;
+                decimal npc1 = new();
+                decimal npc2 = new();
+                DateTime conDeptDate = new();
+                DateTime conArrDate = new();
+                if (distances.ElementAt(0) > 1550)
+                {
+                    // needs a connection flight
+                    string connectionQuery = $"SELECT Distance FROM Distances WHERE (AirportA = \'DEN\' AND AirportB = \'{nda}\') OR (AirportA = \'{nda}\' AND AirportB = \'DEN\') OR (AirportA = 'DEN' AND AirportB = '{noa}') OR (AirportA = '{noa}' AND AirportB = 'DEN')";
+                    SqlCommand connectionCommand = new SqlCommand(connectionQuery, sqlConn);
+                    SqlDataReader connectionReader = connectionCommand.ExecuteReader();
+                    if (!connectionReader.HasRows)
+                    {
+                        Console.WriteLine("Could not find airports distances!");
+                        return;
+                    }
+                    bool allowDEN = true;
+                    while (connectionReader.Read())
+                    {
+                        distances.Add(connectionReader.GetInt32(0));
+                    }
+                    connectionReader.Close();
+                    for (int i = 1; i < distances.Count; i++)
+                    {
+                        if (distances.ElementAt(i) > 1550)
+                        {
+                            allowDEN = false;
+                        }
+                    }
+
+                    if (allowDEN)
+                    {
+                        do
+                        {
+                            Console.WriteLine("Choose a connection airport, MSP or DEN");
+                            con = Console.ReadLine();
+                            if (con == "MSP" | con == "DEN") break;
+                        } while (true);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Must have MSP be connection to the flight since it is too long");
+                        con = "MSP";
+                    }
+                    connectionQuery = $"SELECT Distance FROM Distances WHERE (AirportA = \'MSP\' AND AirportB = \'{nda}\') OR (AirportA = \'{nda}\' AND AirportB = \'MSP\') OR (AirportA = 'MSP' AND AirportB = '{noa}') OR (AirportA = '{noa}' AND AirportB = 'MSP')";
+                    connectionCommand = new SqlCommand(connectionQuery, sqlConn);
+                    connectionReader = connectionCommand.ExecuteReader();
+                    if (!connectionReader.HasRows)
+                    {
+                        Console.WriteLine("Could not find airports distances!");
+                        return;
+                    }
+                    while (connectionReader.Read())
+                    {
+                        distances.Add(connectionReader.GetInt32(0));
+                    }
+                    connectionReader.Close();
+                    decimal firstConPrice;
+                    decimal secondConPrice;
+                    if (con == "DEN")
+                    {
+                        firstConPrice = new decimal(distances.ElementAt(1) * 0.12);
+                        npc1 = firstConPrice;
+                        secondConPrice = new decimal(distances.ElementAt(2) * 0.12);
+                        npc2 = secondConPrice;
+                    }
+                    else
+                    {
+                        firstConPrice = new decimal(distances.ElementAt(3) * 0.12);
+                        npc1 = firstConPrice;
+                        secondConPrice = new decimal(distances.ElementAt(4) * 0.12);
+                        npc2 = secondConPrice;
+                    }
+                    np = decimal.Add(np, firstConPrice);
+                    np = decimal.Add(np, secondConPrice);
+                    npc1 = decimal.Add(npc1, new decimal(8));
+                    npc2 = decimal.Add(npc2, new decimal(8));
+                    np = decimal.Add(np, new decimal(8)); // 8 dollar segment fee for first flight
+                    np = decimal.Add(np, new decimal(8)); // 8 dollar segment fee for connection flight
+
+                    // connection flight dates and times
+                    Console.WriteLine("Input an Arrival Date and Time (mm/dd/yyyy HH:MM:SS) for the first flight");
+                    string? cads = Console.ReadLine();
+                    while (true)
+                    {
+                        if (!DateTime.TryParse(cads, out conArrDate))
+                        {
+                            Console.WriteLine("Could not parse the date and time!");
+                            Console.WriteLine("Input a correct Date and time in the format given no paranthesis please");
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        Console.WriteLine("Input an Arrival Date (mm/dd/yyyy HH:MM:SS) for the first flight");
+                        nads = Console.ReadLine();
+                    }
+                    Console.WriteLine("Input a Departure Date and Time (mm/dd/yyyy HH:MM:SS) for the second flight");
+                    string? cdds = Console.ReadLine();
+                    while (true)
+                    {
+                        if (!DateTime.TryParse(cdds, out conDeptDate))
+                        {
+                            Console.WriteLine("Could not parse the date and time!");
+                            Console.WriteLine("Input a correct Date and time in the format given no paranthesis please");
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        Console.WriteLine("Input a Departure Date (mm/dd/yyyy HH:MM:SS) for the second flight");
+                        nads = Console.ReadLine();
+                    }
+                    // determine the new arrival time
+                    Console.WriteLine("Input an Arrival Date and Time (mm/dd/yyyy HH:MM:SS) for the entire trip");
+                    nads = Console.ReadLine();
+                    while (true)
+                    {
+                        if (!DateTime.TryParse(nads, out nad))
+                        {
+                            Console.WriteLine("Could not parse the date and time!");
+                            Console.WriteLine("Input a correct Date and time in the format given no paranthesis please");
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        Console.WriteLine("Input an Arrival Date (mm/dd/yyyy HH:MM:SS) for the entire trip");
+                        nads = Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    // just a normal flight with a distance
+                    decimal milesAdded = new decimal(distances.ElementAt(0) * 0.12);
+                    np = decimal.Add(np, milesAdded);
+                    np = decimal.Add(np, new decimal(8)); // 8 dollar segment fee
+                }
+                // check the timing of the flights
+                if (ndd.Hour < 8 & ndd.Hour > 5)
+                {
+                    // off-peak discount
+                    np = decimal.Multiply(np, new decimal(.90));
+                }
+                else if (ndd.Hour < 5)
+                {
+                    // red-eye discount
+                    np = decimal.Multiply(np, new decimal(.80));
+                }
+                // check the timing of the flights
+                if (con != null & conArrDate.Hour < 8 & conArrDate.Hour > 5)
+                {
+                    // off-peak discount
+                    npc1 = decimal.Multiply(np, new decimal(.90));
+                }
+                else if (con != null & conArrDate.Hour < 5)
+                {
+                    // red-eye discount
+                    np = decimal.Multiply(np, new decimal(.80));
+                }
+                if (con != null & conDeptDate.Hour < 8 & conDeptDate.Hour > 5)
+                {
+                    // off-peak discount
+                    npc2 = decimal.Multiply(np, new decimal(.90));
+                }
+                else if (con != null & conDeptDate.Hour < 5)
+                {
+                    // red-eye discount
+                    np = decimal.Multiply(np, new decimal(.80));
+                }
                 DateTime endDate = DateTime.Now.AddMonths(7);
                 DateTime departureDate = ndd;
+                DateTime arrivalDate = nad;
                 while (departureDate <= endDate)
                 {
-                    departureDate = departureDate.AddDays(7);
 
                     SqlDateTime sqlDepartureDate = new SqlDateTime(departureDate.Year, departureDate.Month, departureDate.Day, departureDate.Hour, departureDate.Minute, departureDate.Second);
-                    SqlDateTime sqlArrivalDate = new SqlDateTime(departureDate.Year, departureDate.Month, departureDate.Day, nad.Hour, nad.Minute, nad.Second);
+                    SqlDateTime sqlArrivalDate = new SqlDateTime(arrivalDate.Year, arrivalDate.Month, arrivalDate.Day, arrivalDate.Hour, arrivalDate.Minute, arrivalDate.Second);
+                    SqlDateTime sqlConArrDate = new SqlDateTime(conArrDate.Year, conArrDate.Month, conArrDate.Day, conArrDate.Hour, conArrDate.Minute, conArrDate.Second);
+                    SqlDateTime sqlConDeptDate = new SqlDateTime(conDeptDate.Year, conDeptDate.Month, conDeptDate.Day, conDeptDate.Hour, conDeptDate.Minute, conDeptDate.Second);
+                    int? conFlightID1 = null;
+                    int? conFlightID2 = null;
+                    if (con != null) // has a connection flight so create the connection first then link them to the main flight
+                    {
+                        // first flight
+                        string conQueryString = $"INSERT INTO Flights (FlightNumber, OriginCity, DestinationCity, Price, DepartureDateTime, ArrivalDateTime) OUTPUT INSERTED.FlightID " +
+                        $"VALUES ({nfn + 1}, \'{noa}\', \'{con}\', {npc1}, \'{sqlDepartureDate}\', \'{sqlConArrDate}\')";
+                        SqlCommand conQuery = new SqlCommand(conQueryString, sqlConn);
+                        conFlightID1 = (int)conQuery.ExecuteScalar();
+                        // second flight
+                        conQueryString = $"INSERT INTO Flights (FlightNumber, OriginCity, DestinationCity, Price, DepartureDateTime, ArrivalDateTime) OUTPUT INSERTED.FlightID " +
+                        $"VALUES ({nfn + 2}, \'{con}\', \'{nda}\', {npc2}, \'{sqlConDeptDate}\', \'{sqlArrivalDate}\')";
+                        conQuery = new SqlCommand(conQueryString, sqlConn);
+                        conFlightID2 = (int)conQuery.ExecuteScalar();
+                    }
+                    // input the connected flight 
+                    string queryString = $"INSERT INTO Flights (FlightNumber, OriginCity, DestinationCity, Price, DepartureDateTime, ArrivalDateTime, FirstConFlight, SecondConFlight)" +
+                    $"VALUES ({nfn}, \'{noa}\', \'{nda}\', {np}, \'{sqlDepartureDate}\', \'{sqlArrivalDate}\', {conFlightID1}, {conFlightID2})";
+                    SqlCommand query = new SqlCommand(queryString, sqlConn);
+                    int rows = query.ExecuteNonQuery();
 
-                    string queryString = $"INSERT INTO Flights (FlightNumber, OriginCity, DestinationCity, Price, DepartureDateTime, ArrivalDateTime)" +
-                    $"VALUES ({nfn}, \'{noa}\', \'{nda}\', {np}, \'{sqlDepartureDate}\', \'{sqlArrivalDate}\')";                             
-                        SqlCommand query = new SqlCommand(queryString, sqlConn);
-                        int rows = query.ExecuteNonQuery();                      
-                    }  
-                sqlConn.Close();
+                    // add these flights every week
+                    departureDate = departureDate.AddDays(7);
+                    arrivalDate = arrivalDate.AddDays(7);
+                    conArrDate = conArrDate.AddDays(7);
+                    conDeptDate = conDeptDate.AddDays(7);
                 }
-                Console.WriteLine("Flights added for the next 6 months!");                  
+                Console.WriteLine("Flights added for the next 6 months!");
+
+            }
         }
 
         public void DeleteFlight()
